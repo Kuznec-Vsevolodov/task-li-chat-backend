bf57650db62d26be57fe38aee8281ac5d0cbae40
Kuznec-Vsevolodov <andrei.pechurevitch@yandex.ru>
2022-01-18 05:31:11 +0600

    Version 0.7

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// подключение переменных и функций
 const path = require('path');
 const http = require('http');
 const express = require('express');
 const moment = require('moment');
 
 const app = express();
-
+const server = http.createServer(app);
 const io = require('socket.io')(server);
 
 const formatMessage = require('./utils/messages');
@@ -28,6 +27,40 @@ const PORT = 3000 || process.env.PORT;
 var db;
 var team_collection;
 
+function getRandomFinalTask(tasks, user){
+  tasks.sort(()=>Math.random()-0.5)
+  console.log(tasks);
+  console.log(user);
+  if(tasks[0].passed == 1 && tasks[0].user == user){
+    console.log(user);
+    getRandomFinalTask(tasks, user)
+  }else{
+    return tasks[0].text;
+  }
+  // tasks.forEach((task) => {
+  //   console.log(task);
+  //   if(task.passed == 0 && task.user != user){
+  //     console.log(task.text);
+  //     var current_task = task.text;
+  //     console.log(current_task)
+  //     return current_task;
+  //   }
+  // })
+}
+
+function insertGameChanges(is_replay, chat_id, to, ex_user, loose_status, tasks, user, users, type, price, loosed_users){
+  db.collection('game_chats').updateOne( {chat_id: chat_id} , { $set: { user_index: to, users: users, ex_user: ex_user, loose_status: loose_status, tasks: tasks, is_replay: is_replay, loosed_users: loosed_users} } );
+  if(users.length != 1){
+    io.to(user.room).emit('usersChanges', {users: users, user_index: to, price: price, tasks_replay: is_replay});
+  }else if(type != 'sing'){
+    io.to(user.room).emit('finalGame', {final_player: users[to]});
+    db.collection('game_chats').updateOne( {chat_id: chat_id} , { $set: { is_final: true} } );
+    var last_task = getRandomFinalTask(tasks, users[to]);
+    io.to(user.room).emit('message', formatMessage(botName, `Пользователь ${users[to]} вступает в финальный раунд. Его цель - ответить на задание, из-за которого вылетел один из прошлых игроков. Задание таково: "${last_task}". Выполнение задания определяют другие пользователи`, user.room, "chatbot.png", false));
+  }
+  
+  
+}
 
 mongoClient.connect(
   'mongodb://localhost:27017/',  // строка подключения
@@ -45,8 +78,7 @@ mongoClient.connect(
     queue_db = client.db('queue')
 
     // подключение к коллекциям
-    team_collection = db.collection('team_chats');
-    sing_collection = db.collection('sing_chats');
+    chat_collection = db.collection('game_chats');
     team_queue_collection = queue_db.collection('team_queue')
     sing_queue_collection = queue_db.collection('sing_queue');
 
@@ -78,6 +110,126 @@ io.on('connection', socket => {
       io.to(user.room).emit('message', formatMessage(botName, `A ${user.username} has joined the chat`, user.room, "chatbot.png", false));
     });
 
+    socket.on('sendTask', ({task_text, chat_id}) => {
+      const user = getCurrentUser(socket.id);
+      var message_info = formatMessage(user.username, task_text, user.room, user.avatar, false);
+      db.collection('game_chats').findOne({"chat_id": chat_id}, function(err, docs) {
+        if (err){
+          return res.sendStatus(500);
+        }
+        // добавление сообщений в базу
+        docs.tasks.push({text: task_text, user: user.username, passed: null});
+        
+        docs.messages.push(message_info);
+        db.collection('game_chats').updateOne( {chat_id: chat_id} , { $set: { tasks: docs.tasks, messages: docs.messages} } );
+      });
+      // отправка сообщения пользователям комнаты
+      io.to(user.room).emit('message', message_info);
+    });
+
+    socket.on('partyResultHandler', ({value, chat_id}) => {
+      
+      const user = getCurrentUser(socket.id);
+
+      chat_collection.findOne({"chat_id": chat_id}, function(err, result){
+        if(err){ 
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        console.log()
+        if(result.is_final == false){
+          var from_user = result.user_index;
+          var ex_user = result.ex_user;
+          var loose_status = result.loose_status;
+          var users = result.users;
+          var loosed_users = result.loosed_users;
+          var to = 0;
+
+          if(from_user < users.length-1){
+            to = from_user + 1
+          }
+
+          if(value == 1){
+            loose_status = 0;
+            if(result.type == 'sing'){
+              if(ex_user != null){
+                io.to(user.room).emit('finalGameResult', {winner: true, user: result.users[ex_user], price: result.price});
+              }else{
+                io.to(user.room).emit('finalGameResult', {winner: true, user: result.users[to], price: result.price});
+              }
+            }else{
+              if(ex_user != null){
+                loosed_users.push(users[from_user]);
+                users.splice(from_user, 1);
+                ex_user = null;
+                if(from_user > users.length - 1){
+                  from_user = 0
+                }
+                  insertGameChanges(false, chat_id, from_user, ex_user, loose_status, result.tasks, user, users, result.type, result.price, loosed_users);
+                  return('ok')
+              }
+  
+              if(from_user < users.length-1){
+                to = from_user + 1
+              }
+              result.tasks[result.tasks.length-1].passed = 1;
+              ex_user = null;
+              insertGameChanges(false, chat_id, to, ex_user, loose_status, result.tasks, user, users, result.type, result.price, loosed_users);
+              return('ok')
+            }
+           
+          }else if(value == 0){
+            if(loose_status == 1){
+              loose_status = 0;
+              loosed_users.push(users[ex_user]);
+              users.splice(ex_user, 1);
+              to = ex_user;
+              ex_user = null;
+
+              if(users.length == to){
+                to = 0;  
+              }
+              if (result.type == 'sing'){
+                io.to(user.room).emit('finalGameResult', {winner: false, user: result.users[result.user_index], price: result.price});
+              }else{
+                insertGameChanges(false, chat_id, to, ex_user, loose_status, result.tasks, user, users, result.type, result.price, loosed_users);
+              }
+              
+              return('ok')
+            }
+            loose_status = 1;
+            ex_user = from_user;
+
+            from_user = to;
+            result.tasks[result.tasks.length-1].passed = 0;
+            insertGameChanges(true, chat_id, from_user, ex_user, loose_status, result.tasks, user, users, result.type, result.price, loosed_users)
+          }
+        }else if(result.is_final == true){
+          if(value == 1){
+            result.final_positive_score+=1;
+          }else if(value == 0){
+            result.final_negative_score+=1;
+          }
+
+          db.collection('game_chats').updateOne( {chat_id: chat_id} , { $set: { final_positive_score: result.final_positive_score, final_negative_score: result.final_negative_score} } );
+
+          if(result.loosed_users.length <= result.final_negative_score + result.final_positive_score){
+            var winner_defender = true;
+            if(result.final_negative_score > result.final_positive_score){
+              winner_defender = false;
+            }else if(result.final_positive_score == result.final_negative_score){
+              winner_defender = null;
+            }
+            io.to(user.room).emit('message', formatMessage(botName, `${result.users[result.user_index]} победил в игре, заработав при этом ${result.price}`, user.room, 'chatbot.png', false));
+            io.to(user.room).emit('finalGameResult', {winner: winner_defender, user: result.users[result.user_index], price: result.price});
+          }
+          
+        }
+
+      });
+      
+    })
+
     // получение сообщений
     socket.on('chatMessage', ({msg, image}) => {
       // инициализация пользователя
@@ -87,13 +239,13 @@ io.on('connection', socket => {
       var message_info = formatMessage(user.username, msg, user.room, user.avatar, image);
       // поиск сообщений в рамках БД чата
       console.log(user)
-      db.collection(user.game_type+'_chats').findOne({"chat_id": message_info.chat_id}, function(err, docs) {
+      db.collection('game_chats').findOne({"chat_id": message_info.chat_id}, function(err, docs) {
         if (err){
           return res.sendStatus(500);
         }
         // добавление сообщений в базу
         docs.messages.push(message_info);
-        db.collection(user.game_type+'_chats').updateOne( {chat_id: message_info.chat_id} , { $set: { messages: docs.messages } } );
+        db.collection('game_chats').updateOne( {chat_id: message_info.chat_id} , { $set: { messages: docs.messages } } );
       });
       // отправка сообщения пользователям комнаты
       io.to(user.room).emit('message', message_info);
@@ -137,12 +289,22 @@ app.post('/sing-queue', function(req, res){
           var now = new Date();
           var chat = {
             messages: [],
-            user1: data.user1,
-            user2: req.body.username,
+            users: [data.user1, req.body.username],
+            user_index: 0,
+            ex_user: null,
+            loose_status: 0,
+            type: 'sing',
+            tasks: [],
+            is_replay: false,
+            price: req.body.price*2,
+            is_final: false,
+            loosed_users: [],
+            final_positive_score: 0,
+            final_negative_score: 0,
             chat_id: now.getTime()
           }
 
-          sing_collection.insertOne(chat, function(err, chat_res){
+          chat_collection.insertOne(chat, function(err, chat_res){
             if(err){
               return res.send(err)
             }
@@ -156,7 +318,7 @@ app.post('/sing-queue', function(req, res){
                 chat_id: chat_res.ops[0].chat_id
             }
             chat_res.ops[0].messages.push(message);
-            db.collection('sing_chats').updateOne( {chat_id: message.chat_id} , { $set: { messages: chat_res.ops[0].messages } } );
+            db.collection('game_chats').updateOne( {chat_id: message.chat_id} , { $set: { messages: chat_res.ops[0].messages } } );
             // удаление старой очереди  
             sing_queue_collection.remove({user1: data.user1})  
             return res.send({'queue_id': data._id, 'chat_id': chat_res.ops[0].chat_id}) 
@@ -165,9 +327,88 @@ app.post('/sing-queue', function(req, res){
   })
 });
 
-app.post('/getchat-sing', function(req, res) {
+app.post('/team-queue', function(req, res){
+  // проверка на наличие очереди
+  team_queue_collection.findOne({"price": req.body.price}, function(err, data){
+        if (err){
+          return res.sendStatus(500);
+        }
+        if(data == null){
+          // создание очереди
+          var queue = { 
+            users: [req.body.username],
+            price: req.body.price
+          };
+          team_queue_collection.insertOne(queue, function(err, result){
+            if(err){
+              return res.send(err)
+            }
+            // возврат данных
+            return res.send({'queue_id': result.ops[0]._id, 'chat_id': 0}) 
+          });
+        }else{
+          if(data.users.length == 4){
+            // создание чата при наличии пользователя в очереди
+             
+            console.log(data.users);
+            var now = new Date();
+            var users_before_last = data.users;
+            users_before_last = users_before_last;
+            var users = data.users;
+            users.push(req.body.username)
+            var chat = {
+              messages: [],
+              users: users,
+              user_index: 0,
+              ex_user: null,
+              loose_status: 0,
+              type: 'team',
+              tasks: [], 
+              is_replay: false,
+              price: req.body.price*5,
+              is_final: false,
+              loosed_users: [],
+              final_positive_score: 0,
+              final_negative_score: 0,
+              chat_id: now.getTime()
+            }
+
+            chat_collection.insertOne(chat, function(err, chat_res){
+              if(err){
+                return res.send(err)
+              }
+              var message = {
+                  author: "NoOne",
+                  author_avatar: false,
+                  text: "Users: "+users.join(', '),
+                  image: false,
+                  time: moment().format('h:mm a'),
+                  chat_id: chat_res.ops[0].chat_id
+              }
+              chat_res.ops[0].messages.push(message);
+              db.collection('game_chats').updateOne( {chat_id: message.chat_id} , { $set: { messages: chat_res.ops[0].messages } } );
+              // удаление старой очереди  
+              users.pop()
+              console.log(users);
+              team_queue_collection.remove({users: users}); 
+              console.log( data._id) 
+              return res.send({'queue_id': data._id, 'chat_id': chat_res.ops[0].chat_id}) 
+            });  
+
+          }else{
+            var users = data.users;
+            users.push(req.body.username)
+            team_queue_collection.updateOne( {id: data.id} , { $set: { users: users } });
+            console.log(data._id)
+            return res.send({'queue_id': data._id, 'chat_id': 0}) 
+          }
+        }
+  })
+});
+
+app.post('/getchat-data', function(req, res) {
   // получение данных о сообщении
-  sing_collection.findOne({"chat_id": req.body.chat_id}, function(err, result){
+  chat_collection.findOne({"chat_id": req.body.chat_id}, function(err, result){
           
     if(err){ 
         console.log(err);
@@ -177,6 +418,6 @@ app.post('/getchat-sing', function(req, res) {
       return res.sendStatus(500);
     }
     // отправка сообщений
-    res.send(result.messages)
+    res.send(result)
   });
 });
